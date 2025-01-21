c4a423a3ae54af9bacce448526d48cd341fc2314
mizou9494 <betarmizou@gmail.com>
2025-01-21 21:27:44 +0100

    now thr shadcn setup is now up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-
+import { Terminal } from "lucide-react"
+ 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
 function App() {
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>hello</h1>
-    </>
+    <Alert>
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
   )
 }
 
