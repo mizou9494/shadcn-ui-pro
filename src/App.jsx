614bbe7f4077c629c23e99e35f5dcd5b3d0be504
mizou9494 <betarmizou@gmail.com>
2025-01-24 22:34:52 +0100

    just reminding myself of how css works ;p

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ import {
 function App() {
 
   return (
-    <Alert>
+    <Alert className='h-screen w-screen bg-slate-300 grid place-content-center'>
       <Terminal className="h-4 w-4" />
       <AlertTitle>That was just a test</AlertTitle>
       <AlertDescription>
         We will add some additional components soon 👀
       </AlertDescription>
-       <Button>Click me</Button>
+       <Button className="mt-4 p-8 hover:bg-slate-50 hover:text-slate-900" variant="destructive">Click me</Button>
     </Alert>
   )
 }
