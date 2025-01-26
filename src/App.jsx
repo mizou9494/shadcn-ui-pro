1d4e6750a96cb2a47b766c6b69870ed39441b5ac
mizou9494 <betarmizou@gmail.com>
2025-01-26 21:44:59 +0100

    +++

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,16 @@ import {
 function App() {
 
   return (
-    <Alert className='h-screen w-screen bg-slate-300 grid place-content-center'>
-      <Terminal className="h-4 w-4" />
-      <AlertTitle>That was just a test</AlertTitle>
-      <AlertDescription>
-        We will add some additional components soon 👀
-      </AlertDescription>
-       <Button className="mt-4 p-8 hover:bg-slate-50 hover:text-slate-900" variant="destructive">Click me</Button>
-    </Alert>
+    <div className="grid place-content-center h-screen">
+      <Alert className='h-full w-full bg-slate-300 grid place-content-center'>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>That was just a test</AlertTitle>
+        <AlertDescription>
+          We will add some additional components soon 👀
+        </AlertDescription>
+        <Button className="mt-4 p-8 hover:bg-slate-50 hover:text-slate-900 focus:outline-none" variant="destructive">Click me</Button>
+      </Alert>
+    </div>
   )
 }
 
