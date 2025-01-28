3cb5d8378196b4165dffa017e6f4b2fb0d121ba0
mizou9494 <betarmizou@gmail.com>
2025-01-28 23:43:57 +0100

    just a silly commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <AlertDescription>
           We will add some additional components soon 👀
         </AlertDescription>
-        <Button className="mt-4 p-8 hover:bg-slate-50 hover:text-slate-900 focus:outline-none" variant="destructive">Click me</Button>
+        <Button className="mt-4 p-8 hover:bg-slate-150 hover:text-slate-900 focus:outline-none focus:bg-yellow-500 focus:text-black-50" variant="destructive">Click me</Button>
       </Alert>
     </div>
   )
